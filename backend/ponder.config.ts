ade9dfdf834291a6c37683fbcf60e43c656fe08b
Skythrill256 <anjishnuganguly773@gmail.com>
2025-02-05 00:17:13 +0530

    ponder backend

---
+++ backend/ponder.config.ts
@@ -0,0 +1,24 @@
+import { createConfig } from "ponder";
+import { http } from "viem";
+import { AppealContract } from "./abis/AppealContractAbi";
+
+export default createConfig({
+  database: {
+    kind: "postgres",
+    connectionString: process.env.DATABASE_URL
+  },
+  networks: {
+    amoy: {
+      chainId: 80002,
+      transport: http(process.env.RPC_URL)
+    }
+  },
+  contracts: {
+    AppealContract: {
+      network: "amoy",
+      abi: AppealContract,
+      address: "0x900730EA2C10d31f9C8311c0A661aBB9cA09214E",
+      startBlock: 0,
+    },
+  },
+});
